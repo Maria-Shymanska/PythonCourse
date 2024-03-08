46372d9d15e7eee5693cf7f07c6452616c979c63
Maria Shymanska <114136940+Maria-Shymanska@users.noreply.github.com>
2024-03-08 20:25:40 +0100

    add module12

---
+++ module12/csv/03.py
@@ -0,0 +1,14 @@
+import csv
+
+country_codes = {}
+
+filename = "countries.csv"
+
+with open("countries.csv", "r") as f:
+    reader = csv.reader(f)
+    header = next(reader)
+    for line in reader:
+        country_codes[line[0]] = line[1]
+
+print(country_codes)
+print(country_codes["UA"])
